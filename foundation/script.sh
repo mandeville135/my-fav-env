8cbf98e89e0960a8ba494288c675ae1201198f9a
mandeville135 <mandeville135@gmail.com>
2018-09-03 19:07:56 +0000

    Making current

--- foundation/script.sh
+++ foundation/script.sh
@@ -2,3 +2,4 @@
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\] \[\033[33;1m\]\w\[\033[m\] (\$(git branch 2>/dev/null | grep '^*' )) \$ "
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install python-pip
